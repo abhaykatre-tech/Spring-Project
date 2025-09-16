9135127dd317e71a19d646ad09a67259603ebc9a
abhaykatre-tech <katreabhay927@gmail.com>
2025-09-16 13:39:48 +0530

    adding spring-boot project

---
+++ src/main/java/com/example/demo/controller/MicroservceEx.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MicroservceEx {
+
+	@GetMapping("/hello/abhay")
+	public String Hello() {
+		return "hello from 8001 yeh hai micro service ka jadu ";
+	}
+}
